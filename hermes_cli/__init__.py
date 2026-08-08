aa76fdb300cedabe923686192999178dc33ea881
Daniel JB Clark <djbclark@gmail.com>
2026-08-07 21:49:30 -0400

    chore: bump version to v0.19.5 (2026.8.7.4)

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -14,8 +14,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.19.4"
-__release_date__ = "2026.8.7.3"
+__version__ = "0.19.5"
+__release_date__ = "2026.8.7.4"
 
 
 def _ensure_utf8():
